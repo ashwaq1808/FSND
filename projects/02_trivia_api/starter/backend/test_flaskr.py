b599f03c520f246eb6881da5c46aa44a8dc42cee
ashwaq1808 <55363225+ashwaq1808@users.noreply.github.com>
2020-07-03 16:36:47 +0300

    update flask_test

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -24,7 +24,7 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
         pass
@@ -33,6 +33,61 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['categories'])
+
+    def test_get_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['questions'])
+        self.assertEqual(data['total_questions'])
+        self.assertEqual(data['categories'])
+        self.assertEqual(data['current_category'])
+
+    def test_delete_specific_question(self):
+        res = self.client().get('/questions/1')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+
+    def test_create_question(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+
+    def test_search_question(self):
+        res = self.client().get('/search/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['questions'])
+        self.assertEqual(data['total_questions'])
+        self.assertEqual(data['current_category'])
+
+
+    def test_questions_by_category(self):
+        res = self.client().get('/categories/1/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['questions'])
+        self.assertEqual(data['total_questions'])
+
+
+    def test_play_quizzes(self):
+        res = self.client().get('/categories/1/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['questions'])
+        self.assertEqual(data['previous_questions'])
+
+
 
 
 # Make the tests conveniently executable
